3fe8f2af7b26ed17e260b1d7ace57711e4b8bd9c
Roberto Di Stefano <robertodistefano24@gmail.com>
2024-06-06 14:05:17 +0200

    start commit

--- networks/A1.py
+++ networks/A1.py
@@ -5,10 +5,15 @@ class NetA1(nn.Module):
     def __init__(self, num_classes):
         super(NetA1, self).__init__()
         self.relu = nn.ReLU()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=4)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=5)
         self.flatten = nn.Flatten(start_dim=-3)
-        self.linear1 = nn.Linear(2500, num_classes)
+        self.linear1 = nn.Linear(2304, num_classes)
         self.softmax = nn.Softmax(dim=0)
+
+    def freeze_conv_param(self):
+        # Freeze CONV1 parameters
+        for param in self.conv1.parameters():
+            param.requires_grad = False
 
     def forward(self, x):
         x = self.relu(self.conv1(x))
